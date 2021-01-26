3aef655e9b9f9b9020c3319c99fe50924db9f7fa
gambarotto <dgcdeveloper@gmail.com>
2021-01-26 13:52:50 -0300

    BrandController updated

--- src/app/controllers/BrandController.js
+++ src/app/controllers/BrandController.js
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
-import Brand from '../models/Brand';
 import { checkResponsabilityUserToken } from '../middlewares/AuthUser';
 import BrandsValidation from '../../helpers/validations/BrandsValidation';
+import BrandRepositories from '../repositories/BrandRepositories';
 
 class BrandController {
   async store(req, res) {
@@ -12,40 +12,39 @@ class BrandController {
     if (!(await BrandsValidation.store(name, description))) {
       return res.json({ error: 'Please fill all fields' });
     }
-    const brand = await Brand.create({ id: uuidv4(), name, description });
-
-    return res.json({
-      id: brand.id,
-      name: brand.name,
-      description: brand.description,
+    const brand = await BrandRepositories.createBrand({
+      id: uuidv4(),
+      ...req.body,
     });
+
+    return res.json(brand);
   }
   async index(req, res) {
-    const brands = await Brand.findAll({
-      attributes: ['id', 'name', 'description'],
-    });
-    return res.json(brands);
+    try {
+      const brands = await BrandRepositories.findAllBrands();
+      return res.json(brands);
+    } catch (error) {
+      return { error };
+    }
   }
   async update(req, res) {
     if (!(await checkResponsabilityUserToken(req.userId))) {
       return res.json({ error: 'You do not have privileges for do this' });
     }
-    const brand = await Brand.findByPk(req.params.brandId);
-    const { id, name, description } = await brand.update(req.body);
-
-    return res.json({ id, name, description });
-  }
-  async delete(req, res) {
-    const brand = await Brand.findByPk(req.params.brandId);
+    const brand = await BrandRepositories.findBrandById(req.params.brandId);
     if (!brand) {
       return res.json({ error: 'Brand not found' });
     }
-    await brand.destroy();
+    const brandUpdated = await BrandRepositories.updateBrand(brand, req.body);
 
-    return res.json({ message: 'The Brand was deleted' });
+    return res.json(brandUpdated);
+  }
+  async delete(req, res) {
+    const response = await BrandRepositories.deleteBrand(req.params.brandId);
+    return res.json(response);
   }
   async show(req, res) {
-    const brand = await Brand.findByPk(req.params.brandId);
+    const brand = await BrandRepositories.findBrandById(req.params.brandId);
     if (!brand) {
       return res.json({ error: 'Brand not found' });
     }
--- src/app/repositories/BrandRepositories.js
+++ src/app/repositories/BrandRepositories.js
@@ -1,9 +1,35 @@
+import { v4 as uuidv4 } from 'uuid';
 import Brand from '../models/Brand';
 
 class BrandRepositories {
+  static async createBrand(data) {
+    const { id, name, description } = await Brand.create({
+      id: uuidv4(),
+      ...data,
+    });
+    return { id, name, description };
+  }
   static async findBrandById(id) {
     const brand = await Brand.findByPk(id);
     return brand;
+  }
+  static async findAllBrands() {
+    const brands = await Brand.findAll({
+      attributes: ['id', 'name', 'description'],
+    });
+    return brands;
+  }
+  static async updateBrand(brand, data) {
+    const { id, name, description } = await brand.update(...data);
+    return { id, name, description };
+  }
+  static async deleteBrand(id) {
+    const brand = await Brand.findByPk(id);
+    if (!brand) {
+      return { error: 'Brand not found' };
+    }
+    await brand.destroy();
+    return { message: 'The Brand was deleted' };
   }
 }
 export default BrandRepositories;
